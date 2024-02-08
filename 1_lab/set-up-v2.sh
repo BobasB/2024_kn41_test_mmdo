9deb03c4a8ed271055f742cd4fd06f17e840211c
BobasB <bugil.bogdan@gmail.com>
2024-02-08 10:58:50 +0200

    automation for multiple projects

--- 1_lab/set-up-v2.sh
+++ 1_lab/set-up-v2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Заголовок вверху називається shebang
-name="2_project"
+name=$1
+if [[ $name != "1_project" && $name != "2_project" ]]; then
+    echo "Ми передали неправильну назву проекту! В нас є тільки 1_project та 2_project";
+    exit 1
+fi  
 cd $name
 echo "Start creating virtual env in PATH: $(pwd)" # Це команда покаже де ми зараз знаходимось, у якій папці
 python -m venv ./venv_$name # Створення середовища
